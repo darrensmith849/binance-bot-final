672eb454e79df2d9e8c9eaa16b4f1364cd019d31
darrensmith849 <darren@2ko.co.za>
2025-04-11 17:34:43 +0200

    Update binance_momentum_bot_env 3.py

--- binance_momentum_bot_env 3.py
+++ binance_momentum_bot_env 3.py
@@ -7,8 +7,8 @@ from binance.enums import *
 
 # STEP 1: Secure API key loading from environment variables
 
-API_KEY = os.getenv("FCMuPtgOL9h5cwAJOsbvf1klNjiCbZotmppT6SOAjbwaYPeYQajAOoFAu5GUJMsw")
-API_SECRET = os.getenv=("6SEfSmLZg5Q2ruf99NUutPFamAYgELOOqzdtV3sZMRchflRRarnBo9LrHkaDdR1n")
+API_KEY = os.getenv("BINANCE_API_KEY")
+API_SECRET = os.getenv("BINANCE_API_SECRET")
 
 client = Client(API_KEY, API_SECRET)
 
